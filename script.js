ea12161ae5ccd374976a0fe713760519bc91c140
Jenna-Deamer <jennadeamer@gmail.com>
2025-04-07 16:33:46 -0400

    Create skeletons of objs,

---
+++ script.js
@@ -0,0 +1,21 @@
+const gameBoard = (function () {
+    let gameBoard = [
+        rowOne = [0, 1, 2],
+        rowTwo = [3, 4, 5],
+        rowThree = [6, 7, 8]
+    ]
+
+    console.log(gameBoard[0,0])
+    console.log(gameBoard[0,1])
+    console.log(gameBoard[0,2])
+})(); // IIFE since I only need one instance of it!
+
+const gameCOntrol = (function () {
+
+}());
+
+// Player obj
+const createPlayer = function () {
+
+}
+// Create 2 players
