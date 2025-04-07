752bf11df4c5f640b4ef9e41f49624a6e98cfbe0
Jenna-Deamer <jennadeamer@gmail.com>
2025-04-07 16:54:37 -0400

    Add createPLayer function & int two players

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const gameBoard = (function () {
         rowThree = [6, 7, 8]
     ]
 
-    console.log(gameBoard[0,0])
-    console.log(gameBoard[0,1])
-    console.log(gameBoard[0,2])
+    console.log(gameBoard[0, 0])
+    console.log(gameBoard[0, 1])
+    console.log(gameBoard[0, 2])
 })(); // IIFE since I only need one instance of it!
 
 const gameCOntrol = (function () {
@@ -15,7 +15,35 @@ const gameCOntrol = (function () {
 }());
 
 // Player obj
-const createPlayer = function () {
+const createPlayer = function (name, marker, isTurn) {
+    let isWinner = false;
 
-}
+    function setWinner() {
+        return isWinner = true;
+    };
+
+    function switchTurn() {
+        if (isTurn === true) {
+            return isTurn = false;
+        }
+        else {
+            return isTurn = true;
+        }
+    };
+
+    return { name, marker, isTurn, switchTurn, isWinner };
+};
+
 // Create 2 players
+const playerOne = createPlayer("Player 1", "X", true);
+console.log(playerOne.name)
+console.log(playerOne.marker)
+console.log(playerOne.isTurn)
+console.log(playerOne.isWinner)
+
+const playerTwo = createPlayer("Player 2", "O", false);
+console.log(playerTwo.name)
+console.log(playerTwo.marker)
+console.log(playerTwo.isTurn)
+console.log(playerTwo.isWinner)
+
