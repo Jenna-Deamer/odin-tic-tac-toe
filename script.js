c83255e78cce383d041ddb48748a407866281b09
Jenna Deamer <jennadeamer@gmail.com>
2025-04-13 15:57:03 -0400

    Add getCurrentPlayer & skeleton of playGame loop, rewrote playRound to use currentPlayer

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const gameBoard = (function () {
   let gameBoard = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
   ];
 
   const getGameBoard = () => gameBoard;
-  const setGameBoard = () => {
-    gameBoard[selectedRow].splice(selectedCell,1,'X');
+  const setGameBoard = (marker) => {
+    gameBoard[selectedRow].splice(selectedCell, 1, marker);
   };
   const printGameBoard = () => {
     console.log(gameBoard[0]);
@@ -19,25 +19,39 @@ const gameBoard = (function () {
 })(); // IIFE since I only need one instance of it!
 
 const gameController = (function () {
+  let currentPlayer;
   gameBoard.getGameBoard();
-  gameBoard.printGameBoard();
+  //   gameBoard.printGameBoard();
 
-  // Play a round
-  const playRound = (playerOne, playerTwo) => {
-    // Determine whose turn it is
+  const getCurrentPlayer = () => {
     if (playerOne.getTurn() === true) {
-      console.log("It's " + playerOne.name + " turn.");
-      // Push X to selected cell
-      gameBoard.setGameBoard(playerOne, selectedCell, selectedRow);
-      // Print updated game board
-      gameBoard.printGameBoard();
-      // switch turn
-      playerOne.setTurn();
+      return (currentPlayer = playerOne);
     } else {
-      console.log("It's " + playerTwo.name + " turn.");
+      return (currentPlayer = playerTwo);
     }
   };
-  return { playRound };
+
+  // Play a round
+  const playRound = () => {
+    let marker = currentPlayer.getMarker();
+    gameBoard.setGameBoard(marker, selectedCell, selectedRow);
+    // Print updated board
+    // gameBoard.printGameBoard();
+    // Switch turn
+    
+    currentPlayer.setTurn();
+    console.log(currentPlayer.name + "'s turn is over.");
+    console.log(currentPlayer.name + currentPlayer.isTurn);
+  };
+
+  const playGame = () =>{
+    for(let i = 0; i < 3; i++){
+        console.log("Round " + i);
+        getCurrentPlayer();
+        playRound();
+    }
+  }
+  return { playGame, getCurrentPlayer };
 })();
 
 // Player obj
@@ -59,20 +73,27 @@ const createPlayer = function (name, marker) {
     }
   };
 
-  return { name, marker, setTurn, getTurn, setWinnerStatus, getWinnerStatus };
+  const getMarker = () => marker;
+  return {
+    name,
+    getMarker,
+    setTurn,
+    getTurn,
+    setWinnerStatus,
+    getWinnerStatus,
+  };
 };
 
 // Create 2 players
 const playerOne = createPlayer("Player 1", "X");
-console.log(playerOne.name);
-console.log(playerOne.marker);
-console.log("It's player one's turn: " + playerOne.setTurn());
-console.log("Has Won: " + playerOne.getWinnerStatus());
-
+// Player one starts first.
+playerOne.setTurn();
 const playerTwo = createPlayer("Player 2", "O");
-console.log(playerTwo.name);
-console.log(playerTwo.marker);
-console.log("It's player two's turn: " + playerTwo.getTurn());
-console.log("Has Won: " + playerTwo.getWinnerStatus());
 
-gameController.playRound(playerOne, playerTwo);
+// Start game
+selectedCell = 0;
+selectedRow = 1;
+
+
+gameController.playGame();
+
